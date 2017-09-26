10c5e66c2b0f008554fcc3a09930105c19794d63
mattwilson1024 <matt@matt-wilson.org>
2017-09-26 09:13:48 +0100

    Show router areas and add description text

--- src/app-ng1/app.ts
+++ src/app-ng1/app.ts
@@ -11,7 +11,4 @@ angular.module('example', [
   // Restore the hash-prefix to /#/ instead of /#!/ to remain backwards compatible with the pre-1.6 routing - see https://docs.angularjs.org/guide/migration#commit-aa077e8
   $locationProvider.hashPrefix('');
 }])
-.config(['$routeProvider', function($routeProvider) {
-  
-}])
 .directive('appRoot', downgradeComponent({component: AppComponent}));
